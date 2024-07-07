f1d4fce68defc09ca1a81f424474c67dba21499c
Isliwei <49444387+Isliwei@users.noreply.github.com>
2024-07-07 18:12:15 +0800

    xx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/dist/',
+  // base: '/',
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
